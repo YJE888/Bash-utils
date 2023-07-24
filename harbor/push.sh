c4f663f117bb1bca23d32c4acc0494f99fa37ad3
YJE888 <75539276+YJE888@users.noreply.github.com>
2023-07-24 10:38:23 +0900

    Update push.sh

--- harbor/push.sh
+++ harbor/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 실행중인 컨테이너의 현재 상태 그대로 commit 후 harbor에 이미지 push
 # worker노드마다 /root/repo에 harbor패스워드 저장된 파일 존재해야됨
-# {{ HARBOR_HOST_입력 }} 부분에 host명 입력 ex) test-repo.com
+# #harbor-domain_입력 부분에 host명 입력 ex) test-repo.com
 if [ $1 = "-h" ] || [ $1 = "--help" ]
 then
         echo -e "Usage : ./push.sh {NS} {DEPLOY_NAME} {IMG_Name} {TAG}"
@@ -23,6 +23,6 @@ NODE=$(kubectl get pods ${POD_FULL_NAME} -n ${NS} -o jsonpath={.spec.nodeName})
 CID=$(kubectl describe pod ${POD_FULL_NAME} -n ${NS} | grep "Container ID" | rev | cut -c -64 | rev)
 
 # pod가 실행 중인 node에 접속하여 컨테이너ID를 사용하여 commit 후 harbor에 push
-ssh ${NODE} "docker commit ${CID} {{ HARBOR_HOST_입력 }}/${NS}/${REPO}:${TAG};cat /root/repo/my_password.txt | docker login {{ HARBOR_HOST_입력 }} --username admin --password-stdin;docker push {{ HARBOR_HOST_입력 }}/${NS}/${REPO}:${TAG};docker logout {{ HARBOR_HOST_입력 }};exit;"
+ssh ${NODE} "docker commit ${CID} #harbor-domain_입력/${NS}/${REPO}:${TAG};cat /root/repo/my_password.txt | docker login #harbor-domain_입력 --username admin --password-stdin;docker push # harbor-domain_입력/${NS}/${REPO}:${TAG};docker logout # harbor-domain_입력;exit;"
 
 exit 0  
